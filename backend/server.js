773ce241ea71071055d1b96dbe1278f48aeda4c5
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-10-21 11:16:01 +0200

    added express.js backend

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+const answers = [
+  'It is certain',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes definitely',
+  'You may rely on it',
+  'As I see it, yes',
+  'Most likely',
+  'Outlook good',
+  'Yes',
+  'Signs point to yes',
+  'Reply hazy, try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  "Don't count on it",
+  'My reply is no',
+  'My sources say no',
+  'Outlook not so good',
+  'Very doubtful',
+];
+
+app.get('/answer', (req, res) => {
+  const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+  res.json({ answer: randomAnswer });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
